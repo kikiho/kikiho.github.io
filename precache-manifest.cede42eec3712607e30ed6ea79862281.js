decf8cb2354ead0db40297f8cf041621958d9f19
Kiki Ho <ahkiho@students.cs.ubc.ca>
2020-01-19 19:13:30 -0800

    Updates

---
+++ precache-manifest.cede42eec3712607e30ed6ea79862281.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa5342f0e740dc22856fa68cedf7f1a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5f2d918e433ba6c6dde",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "7a7a431d01e944658f28",
+    "url": "/static/js/2.d29e7275.chunk.js"
+  },
+  {
+    "revision": "a1afa5f7c8dff2a316c41be2103336f5",
+    "url": "/static/js/2.d29e7275.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e5f2d918e433ba6c6dde",
+    "url": "/static/js/main.5917450a.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  }
+]);
